4c8669531aa5ec9ab9890b821c305208efdac099
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-13 20:40:39 +0700

    remove: firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const App: React.FC<IProps>=() =>{
+interface IProps {}
+
+const App: React.FC<IProps> = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
-export default App
+export default App;
