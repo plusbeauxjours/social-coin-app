fa3f5f55f254da887c893e7903cc9073170f9db6
plusbeauxjours <plusbeauxjours@gmail.com>
2022-02-13 20:58:22 +0700

    set: reactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./Root";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 interface IProps {}
 
+const queryClient = new QueryClient();
+
 const App: React.FC<IProps> = () => {
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
